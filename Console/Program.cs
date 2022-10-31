de62ad5156147c993a20cf39851113b74afa34f6
Doug Waldron <douglas.waldron@dnr.ga.gov>
2022-10-31 10:16:45 -0400

    Update CLI settings

--- Console/Program.cs
+++ Console/Program.cs
@@ -2,22 +2,31 @@
 
 namespace CheckServerSetup;
 
-internal class Program
+internal static class Program
 {
     /// <param name="email">An email recipient to use with testing the email 
     /// setup (in addition to those in the appsettings.json file).</param>
-    private static async Task Main(string email)
+    /// <param name="env">Optional environment name to load an alternate appsettings.{env}.json file.</param>
+#pragma warning disable S1144
+    // ReSharper disable once UnusedMember.Local
+    private static async Task Main(string email, string env)
+#pragma warning restore S1144
     {
-        var _checkDatabaseOptions = new CheckDatabaseOptions();
-        var _checkEmailOptions = new CheckEmailOptions();
-        var _checkExternalServicesOptions = new CheckExternalServicesOptions();
+        var checkDatabaseOptions = new CheckDatabaseOptions();
+        var checkEmailOptions = new CheckEmailOptions();
+        var checkExternalServicesOptions = new CheckExternalServicesOptions();
 
         try
         {
-            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-            config.Bind(nameof(CheckDatabaseOptions), _checkDatabaseOptions);
-            config.Bind(nameof(CheckEmailOptions), _checkEmailOptions);
-            config.Bind(nameof(CheckExternalServicesOptions), _checkExternalServicesOptions);
+            var config = string.IsNullOrEmpty(env)
+                ? new ConfigurationBuilder().AddJsonFile("appsettings.json").Build()
+                : new ConfigurationBuilder().AddJsonFile($"appsettings.{env}.json").Build();
+
+#pragma warning disable IL2026
+            config.Bind(nameof(CheckDatabaseOptions), checkDatabaseOptions);
+            config.Bind(nameof(CheckEmailOptions), checkEmailOptions);
+            config.Bind(nameof(CheckExternalServicesOptions), checkExternalServicesOptions);
+#pragma warning restore IL2026
         }
         catch (Exception ex)
         {
@@ -27,18 +36,18 @@ internal class Program
 
         if (!string.IsNullOrWhiteSpace(email))
         {
-            _checkEmailOptions.Recipients = _checkEmailOptions.Recipients is null
+            checkEmailOptions.Recipients = checkEmailOptions.Recipients is null
                 ? new[] { email }
-                : _checkEmailOptions.Recipients.Append(email).ToArray();
+                : checkEmailOptions.Recipients.Append(email).ToArray();
         }
 
         AnsiConsole.WriteLine();
         AnsiConsole.Write(new Rule(Emoji.Known.Clipboard + $" Checking setup on {Environment.MachineName}.")
             .LeftAligned().RuleStyle("yellow bold"));
 
-        await CheckDatabase.ExecuteAsync(_checkDatabaseOptions);
-        await CheckEmail.ExecuteAsync(_checkEmailOptions);
-        await CheckExternalServices.ExecuteAsync(_checkExternalServicesOptions);
+        await CheckDatabase.ExecuteAsync(checkDatabaseOptions);
+        await CheckEmail.ExecuteAsync(checkEmailOptions);
+        await CheckExternalServices.ExecuteAsync(checkExternalServicesOptions);
 
         AnsiConsole.WriteLine();
         AnsiConsole.WriteLine("Press any key to continue.");
