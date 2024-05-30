4aaf064e45007b334cc8d55703ad5492f638cee0
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-05-30 08:52:46 -0400

    Update Raygun package

--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -4,10 +4,10 @@ using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
+using Mindscape.Raygun4Net;
 using Mindscape.Raygun4Net.AspNetCore;
 using System.Runtime.InteropServices;
 using WebApp.Platform;
-using WebApp.Platform.Raygun;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -60,8 +60,23 @@ if (!builder.Environment.IsDevelopment()) builder.Services.AddHsts(opts => opts.
 // Configure application monitoring.
 if (!string.IsNullOrEmpty(ApplicationSettings.RaygunSettings.ApiKey))
 {
-    builder.Services.AddRaygun(builder.Configuration,
-        new RaygunMiddlewareSettings { ClientProvider = new RaygunClientProvider() });
+    // Add error logging
+    builder.Services.AddSingleton(s =>
+    {
+        var client = new RaygunClient(s.GetService<RaygunSettings>()!, s.GetService<IRaygunUserProvider>()!);
+        client.SendingMessage += (_, eventArgs) =>
+        {
+            eventArgs.Message.Details.Tags.Add(builder.Environment.EnvironmentName);
+        };
+        return client;
+    });
+    builder.Services.AddRaygun(builder.Configuration, opts =>
+    {
+        opts.ApiKey = ApplicationSettings.RaygunSettings.ApiKey;
+        opts.ExcludeErrorsFromLocal = false;
+        opts.IgnoreFormFieldNames = ["*Password"];
+    });
+    builder.Services.AddRaygunUserProvider();
     builder.Services.AddHttpContextAccessor(); // needed by RaygunScriptPartial
 }
 
