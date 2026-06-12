bb1b38f1983fa534ad0a20060a16ac0c7394ce42
Doug Waldron <douglas.waldron@dnr.ga.gov>
2026-06-12 07:30:04 -0400

    Remove Raygun

--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -2,8 +2,6 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
-using Mindscape.Raygun4Net;
-using Mindscape.Raygun4Net.AspNetCore;
 using ServerCheckupLibrary.Checks;
 using ServerCheckupLibrary.Hubs;
 using WebApp.Platform;
@@ -22,7 +20,6 @@ builder.Configuration.GetSection(nameof(CheckExternalServiceOptions)).Bind(AppSe
 builder.Configuration.GetSection(nameof(CheckDotnetVersionOptions)).Bind(AppSettings.CheckDotnetVersionOptions);
 builder.Configuration.GetSection(nameof(DevOptions)).Bind(AppSettings.DevOptions);
 AppSettings.ServerName = builder.Configuration.GetValue<string>(nameof(AppSettings.ServerName)) ?? "Unknown";
-builder.Configuration.GetSection(nameof(AppSettings.RaygunSettings)).Bind(AppSettings.RaygunSettings);
 
 // Configure authentication.
 if (AppSettings.DevOptions.UseLocalAuth)
@@ -49,28 +46,6 @@ builder.Services.AddSignalR();
 // Configure HSTS (max age: two years).
 if (!isDevelopment) builder.Services.AddHsts(opts => opts.MaxAge = TimeSpan.FromDays(730));
 
-// Configure application monitoring.
-if (!string.IsNullOrEmpty(AppSettings.RaygunSettings.ApiKey))
-{
-    // Add error logging
-    builder.Services
-        .AddSingleton(s =>
-        {
-            var client = new RaygunClient(s.GetService<RaygunSettings>()!, s.GetService<IRaygunUserProvider>()!);
-            client.SendingMessage += (_, eventArgs) =>
-                eventArgs.Message.Details.Tags.Add(builder.Environment.EnvironmentName);
-            return client;
-        })
-        .AddRaygun(builder.Configuration, opts =>
-        {
-            opts.ApiKey = AppSettings.RaygunSettings.ApiKey;
-            opts.ExcludeErrorsFromLocal = false;
-            opts.IgnoreFormFieldNames = ["*Password"];
-        })
-        .AddRaygunUserProvider()
-        .AddHttpContextAccessor(); // needed by RaygunScriptPartial
-}
-
 // Configure the UI.
 builder.Services.AddRazorPages().AddMicrosoftIdentityUI();
 builder.Services.AddWebOptimizer(minifyJavaScript: !isDevelopment);
@@ -86,7 +61,6 @@ if (isDevelopment) app.UseDeveloperExceptionPage(); // Development
 else app.UseExceptionHandler("/Error"); // Production or Staging
 
 // Configure the HTTP request pipeline.
-if (!string.IsNullOrEmpty(AppSettings.RaygunSettings.ApiKey)) app.UseRaygun();
 app
     .UseStatusCodePages()
     .UseHttpsRedirection()
